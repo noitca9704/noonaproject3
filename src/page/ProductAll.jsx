9eced37fb611618b3f49e18a7d31db290920b297
noitca9704 <yy002756@naver.com>
2025-04-09 20:21:04 +0900

    4th commit

--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -6,7 +6,7 @@ const ProductAll = () => {
   const [productList, setProductList] = useState([])
 
   const getProducts = async () => {
-    let url = 'http://localhost:5000/products'
+    let url = 'https://my-json-server.typicode.com/noitca9704/noonaproject3/products'
     let response = await fetch(url)
     let data = await response.json()
     setProductList(data)
